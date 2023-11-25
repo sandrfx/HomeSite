1b0519f77064ed4c9f734f96a48a93694478bb5a
Aleksandr Bilchuk <aabilchuk@avito.ru>
2023-11-26 00:34:33 +0300

    new brand homepage

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -14,25 +14,9 @@
                 analytics.logEvent('navButtonProjects_click');
                 console.log('navButtonProjects_click event sent');
             });
-            $('#navButtonEvents').click(function() {
-                analytics.logEvent('navButtonProjectsEvents_click');
-                console.log('navButtonProjectsEvents_click event sent');
-            });
             $('#navButtonContacts').click(function() {
                 analytics.logEvent('navButtonContacts_click');
                 console.log('navButtonContacts_click event sent');
-            });
-            $('#btnAbout').click(function() {
-                analytics.logEvent('buttonAbout_click');
-                console.log('buttonAbout_click event sent');
-            });
-            $('#btnCV').click(function() {
-                analytics.logEvent('buttonCV_click');
-                console.log('buttonCV_click event sent');
-            });
-            $('#linkTelegram').click(function() {
-                analytics.logEvent('linkTelegram_click');
-                console.log('linkTelegram_click event sent');
             });
             $('#linkEmail').click(function() {
                 analytics.logEvent('linkEmail_click');
@@ -45,10 +29,6 @@
             $('#linkGitHub').click(function() {
                 analytics.logEvent('linkGitHub_click');
                 console.log('linkGitHub_click event sent');
-            });
-            $('#linkFacebook').click(function() {
-                analytics.logEvent('linkFacebook_click');
-                console.log('linkFacebook_click event sent');
             });
             $('#linkLinkednIn').click(function() {
                 analytics.logEvent('linkLinkednIn_click');
